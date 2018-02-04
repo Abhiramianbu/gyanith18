6ad6a4d519efc39f4452881a23a315afd812556e
Abhiramianbu <vmkaabhirami@gmail.com>
2018-02-05 01:38:50 +0530

    landing page template

---
+++ index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Gyanith|2018</title>
+	<?php include "meta.php" ?>
+</head>
+<body>
+	<video autoplay muted loop id="myVideo">
+	  <source src="" type="video/mp4">
+	</video>
+	<section id="dw_wrapper" class="clearfix">
+		<section id="dw_countdown" class="clearfix">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				    <h1 class="top-h1">NIT Puducherry</h1>
+					<img src="images/logo.png" class="site-logo">
+					<h1 class="dw_launch_header">Going Live In</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="clock-container">
+					<center><div class="dw_clock" id="screen-flipclock"></div></center>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p class="dw_launch_subtext">We will let you know when we are launching.</p>	
+				</div>
+			</div>	
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<section id="dw_mailchimp_form">
+						<div id="mc_embed_signup" class="clearfix">
+							<form action="subscribe.php" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate>
+								<div class="mc-field-group">
+									<input type="email" name="email" class="required email" id="mce-EMAIL" placeholder="Enter your email">
+									<div class="subscribe-btn">
+										<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+									</div>
+								</div>
+							</form>
+						</div>
+					</section>
+				</div>
+			</div>
+		
+		</section>
+		<section id="dw_tut_credits">
+
+		</section>
+	</section>
+	<div class="row" id="footer-section">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<footer>
+			<div class="icon-cont">
+				<a href="https://goo.gl/KqxJ6u" target="_blank"><i class="fab fa-facebook"></i></a>
+				<a href="https://goo.gl/9x7ccG" target="_blank"><i class="fab fa-twitter"></i></a>
+				<a href="https://goo.gl/wbJ842" target="_blank"><i class="fab fa-instagram"></i></a>
+			</div>
+		</footer>
+		<hr class="end-section-hr">
+		<section class="end-section">
+			<p>Made with <i class="fas fa-heart"></i> by <a href="#" id="anchor">Web Team</a> | NIT Puducherry</p>
+		</section>
+	</div>
+</div>
+</body>
+<script	src="js/jquery-2.2.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/flipclock.min.js"></script>
+<script src="js/scripts.js"></script>
+</html>
---
+++ meta.php
@@ -0,0 +1,5 @@
+	<link href="css/fontawesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link href="fontsgoogleapis.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/flipclock.css">
