2d54dd1351d796cf217376b92514f18dc1d3d5ca
Abhiramianbu <vmkaabhirami@gmail.com>
2018-02-08 22:59:33 +0530

    Landing page completed.

--- index.php
+++ index.php
@@ -3,56 +3,42 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Gyanith|2018</title>
+	<title>Gyanith | 2018</title>
 	<?php include "meta.php" ?>
 </head>
 <body>
-	<video autoplay muted loop id="myVideo">
-	  <source src="" type="video/mp4">
-	</video>
+<div class="login-wrap">
 	<section id="dw_wrapper" class="clearfix">
 		<section id="dw_countdown" class="clearfix">
 			<div class="row">
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 				    <h1 class="top-h1">NIT Puducherry</h1>
-					<img src="images/logo.png" class="site-logo">
-					<h1 class="dw_launch_header">Going Live In</h1>
+					<img src="images/gynshadow.svg" class="site-logo">
 				</div>
 			</div>
+			<div class="row" style="height:95px;"></div>
 			<div class="row">
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="clock-container">
+					<br/><h1 class="dw_launch_header"  style="margin-bottom:-12px;">Going Live In</h1>
 					<center><div class="dw_clock" id="screen-flipclock"></div></center>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-					<p class="dw_launch_subtext">We will let you know when we are launching.</p>	
-				</div>
-			</div>	
-			<div class="row">
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-					<section id="dw_mailchimp_form">
-						<div id="mc_embed_signup" class="clearfix">
-							<form action="subscribe.php" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate>
-								<div class="mc-field-group">
-									<input type="email" name="email" class="required email" id="mce-EMAIL" placeholder="Enter your email">
-									<div class="subscribe-btn">
-										<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-									</div>
-								</div>
-							</form>
-						</div>
-					</section>
-				</div>
-			</div>
-		
 		</section>
 		<section id="dw_tut_credits">
 
 		</section>
 	</section>
+	
 	<div class="row" id="footer-section">
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<hr class="end-section-hr">
+		<div class="col-lg-3 col-md-3 col-centered"></div>
+		<div class="col-lg-6 col-md-6 col-centered">
+		<section class="end-section">
+			<p>Made with <i class="fas fa-heart"></i> by <a href="#" id="anchor">Web Team</a> | NIT Puducherry</p>
+		</section>
+		</div>
+
+		<div class="col-lg-3 col-md-3 col-centered">
 		<footer>
 			<div class="icon-cont">
 				<a href="https://goo.gl/KqxJ6u" target="_blank"><i class="fab fa-facebook"></i></a>
@@ -60,10 +46,9 @@
 				<a href="https://goo.gl/wbJ842" target="_blank"><i class="fab fa-instagram"></i></a>
 			</div>
 		</footer>
-		<hr class="end-section-hr">
-		<section class="end-section">
-			<p>Made with <i class="fas fa-heart"></i> by <a href="#" id="anchor">Web Team</a> | NIT Puducherry</p>
-		</section>
+		</div>
+
+<!--	</div>-->
 	</div>
 </div>
 </body>
@@ -71,4 +56,12 @@
 <script src="js/bootstrap.min.js"></script>
 <script src="js/flipclock.min.js"></script>
 <script src="js/scripts.js"></script>
+<!--
+<script src="js/jquery.backgroundMove.js"></script>
+<script type="text/javascript">
+$('body').backgroundMove({
+  movementStrength:'5'
+});
+</script>
+-->
 </html>
