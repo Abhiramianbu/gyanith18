ed83cf890dc59c8295c7e440157f1b24b2a7315c
Abhiramianbu <vmkaabhirami@gmail.com>
2018-02-10 14:15:25 +0530

    footer fix

--- index.php
+++ index.php
@@ -5,63 +5,45 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Gyanith | 2018</title>
 	<?php include "meta.php" ?>
+	<script	src="js/jquery-2.2.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/flipclock.min.js"></script>
+	<script src="js/scripts.js"></script>
 </head>
 <body>
-<div class="login-wrap">
 	<section id="dw_wrapper" class="clearfix">
-		<section id="dw_countdown" class="clearfix">
-			<div class="row">
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				    <h1 class="top-h1">NIT Puducherry</h1>
-					<img src="images/gynshadow.svg" class="site-logo">
-				</div>
+		<div class="row" id="logo-wrap">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			    <h1 class="top-h1">NIT Puducherry</h1>
+					<!--<img src="images/gynshadow.svg" class="site-logo">-->
+					<?php include "logo.php";?>
 			</div>
-			<div class="row" style="height:95px;"></div>
-			<div class="row">
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="clock-container">
-					<br/><h1 class="dw_launch_header"  style="margin-bottom:-12px;">Going Live In</h1>
-					<center><div class="dw_clock" id="screen-flipclock"></div></center>
-				</div>
+		</div>
+		<div class="row" style="height:20%;"></div>
+		<div class="row" id="timer-wrap" >
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="clock-container">
+				<h1 class="dw_launch_header"  style="margin-bottom:-12px;">Going Live In</h1>
+				<center><div class="dw_clock" id="screen-flipclock"></div></center>
 			</div>
-		</section>
-		<section id="dw_tut_credits">
-
-		</section>
+		</div>
 	</section>
 	
 	<div class="row" id="footer-section">
 		<hr class="end-section-hr">
-		<div class="col-lg-3 col-md-3 col-centered"></div>
-		<div class="col-lg-6 col-md-6 col-centered">
-		<section class="end-section">
-			<p>Made with <i class="fas fa-heart"></i> by <a href="#" id="anchor">Web Team</a> | NIT Puducherry</p>
-		</section>
+		<div class="col-lg-3 col-md-3 col-sm-3 col-centered">
+			<footer>
+				<div class="icon-cont">
+					<a href="https://goo.gl/KqxJ6u" target="_blank"><i class="fab fa-facebook"></i></a>
+					<a href="https://goo.gl/9x7ccG" target="_blank"><i class="fab fa-twitter"></i></a>
+					<a href="https://goo.gl/wbJ842" target="_blank"><i class="fab fa-instagram"></i></a>
+				</div>
+			</footer>
 		</div>
-
-		<div class="col-lg-3 col-md-3 col-centered">
-		<footer>
-			<div class="icon-cont">
-				<a href="https://goo.gl/KqxJ6u" target="_blank"><i class="fab fa-facebook"></i></a>
-				<a href="https://goo.gl/9x7ccG" target="_blank"><i class="fab fa-twitter"></i></a>
-				<a href="https://goo.gl/wbJ842" target="_blank"><i class="fab fa-instagram"></i></a>
-			</div>
-		</footer>
+		<div class="col-lg-6 col-md-6 col-sm-6 col-lg-offset-right-3 col-md-offset-right-3 col-sm-offset-right-3 col-centered"> 
+			<section class="end-section">
+				<p><a href="#" id="anchor">Made with <i class="fas fa-heart"></i> by <span>Web Team Gyanith</span> | NIT Puducherry</a> </p>
+			</section>
 		</div>
-
-<!--	</div>-->
 	</div>
-</div>
 </body>
-<script	src="js/jquery-2.2.4.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/flipclock.min.js"></script>
-<script src="js/scripts.js"></script>
-<!--
-<script src="js/jquery.backgroundMove.js"></script>
-<script type="text/javascript">
-$('body').backgroundMove({
-  movementStrength:'5'
-});
-</script>
--->
 </html>
